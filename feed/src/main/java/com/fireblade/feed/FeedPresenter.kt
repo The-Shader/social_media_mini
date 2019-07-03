cc13801a7bfe54eb84ba85ee03e1e93d622ee37e
David Tverdota <fireblade.sg@gmail.com>
2019-07-03 22:49:13 +0100

    Changing to Scheduler injection in the Feed module.

--- feed/src/main/java/com/fireblade/feed/FeedPresenter.kt
+++ feed/src/main/java/com/fireblade/feed/FeedPresenter.kt
@@ -1,37 +1,38 @@
 package com.fireblade.feed
 
+import com.fireblade.core.post.PostItem
+import com.fireblade.core.schedulers.ISchedulers
 import com.fireblade.persistence.user.AvatarColor
 import com.fireblade.persistence.SocialMediaRepository
 import com.fireblade.persistence.comment.Comment
 import com.fireblade.persistence.post.Post
 import com.fireblade.persistence.user.User
 import io.reactivex.Flowable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Function3
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class FeedPresenter @Inject constructor(
   private val view: IFeedView,
-  private val socialMediaRepository: SocialMediaRepository
+  private val socialMediaRepository: SocialMediaRepository,
+  private val schedulers: ISchedulers
 ) : IFeedPresenter {
 
   private val subscribers = CompositeDisposable()
 
   override fun loadPostItems() {
 
-    val users = socialMediaRepository.getUsers().observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
-    val posts = socialMediaRepository.getPosts().observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
-    val comments = socialMediaRepository.getComments().observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io())
+    val users = socialMediaRepository.getUsers().observeOn(schedulers.androidMain()).subscribeOn(schedulers.io())
+    val posts = socialMediaRepository.getPosts().observeOn(schedulers.androidMain()).subscribeOn(schedulers.io())
+    val comments = socialMediaRepository.getComments().observeOn(schedulers.androidMain()).subscribeOn(schedulers.io())
 
     Flowable.zip(users, posts, comments,
-      Function3<List<User>, List<Post>, List<Comment>, List<com.fireblade.core.post.PostItem>> { userList, postList, commentList ->
+      Function3<List<User>, List<Post>, List<Comment>, List<PostItem>> { userList, postList, commentList ->
         return@Function3 createPostItems(userList, postList, commentList)
       }
     )
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribeOn(Schedulers.io())
+      .observeOn(schedulers.androidMain())
+      .subscribeOn(schedulers.io())
       .subscribe(view::setPostItems, view::handleError)
       .apply {
         subscribers.add(this)
@@ -42,12 +43,12 @@ class FeedPresenter @Inject constructor(
     subscribers.clear()
   }
 
-  private fun createPostItems(users: List<User>, posts: List<Post>, comments: List<Comment>) : List<com.fireblade.core.post.PostItem> {
+  private fun createPostItems(users: List<User>, posts: List<Post>, comments: List<Comment>) : List<PostItem> {
 
     return posts.map { post ->
 
       val user = users.find { user -> user.id == post.userId }
-      com.fireblade.core.post.PostItem(
+      PostItem(
         post.postId,
         post.title,
         post.body,
